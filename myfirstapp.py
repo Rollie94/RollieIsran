686db49562b2562093f0744f43553fdc6df67bad
Rollie94 <154412108+Rollie94@users.noreply.github.com>
2023-12-20 16:47:51 +0800

    myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -12,7 +12,7 @@ if option=='line chart':
     chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
- st.line_chart(chart_data)
+st.line_chart(chart_data)
 
 elif option=='map':
     map_data = pd.DataFrame(
