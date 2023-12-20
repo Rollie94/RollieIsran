b619bf8a72f5bfe5628831fd1a82ab4567bcb3f5
Rollie94 <154412108+Rollie94@users.noreply.github.com>
2023-12-20 16:45:52 +0800

    myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -13,16 +13,28 @@ if option=='line chart':
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
  st.line_chart(chart_data)
+
 elif option=='map':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
-st.map(map_data)
+    st.map(map_data)
 
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox(' I agree the terms and condition')
-if show:
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
         st.write(pd.DataFrame({
-    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-    'Attendance Status': ['yes', 'yes', 'yes', 'no']
-}))
+          'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+          'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        }))
+elif option=='Long Process':
+    'Starting a long computation...'
+
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
