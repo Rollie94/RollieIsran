f752f149188cbcc4ac228a244c8d11d31f845234
Rollie94 <154412108+Rollie94@users.noreply.github.com>
2023-12-20 16:38:24 +0800

    myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -18,6 +18,7 @@ elif option=='map':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
+st.map(map_data)
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox(' I agree the terms and condition')
